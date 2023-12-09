532bb3037d10e8851666d2dcc6153e28e01cbab7
Damilare Elijah ISMAILA <bestbynature2010@gmail.com>
2023-12-09 21:25:34 +0000

    feat: Update server.js
    Update fccTesting, render method, session, and passport in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
+const { ObjectID } = require("mongodb");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
@@ -10,29 +11,41 @@ app.set("view engine", "pug");
 
 app.set("views", "./views/pug");
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app);
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route("/").get((req, res) => {
-  res.render("index", { title: 'Hello', message: 'Please log in' });
+  res.render("index", { title: "Hello", message: "Please log in" });
 });
 
-const session = require('express-session');
-const passport = require('passport');
+const session = require("express-session");
+const passport = require("passport");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
 
 app.use(passport.initialize());
 
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, doc);
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //   done(null, doc);
+  // });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
