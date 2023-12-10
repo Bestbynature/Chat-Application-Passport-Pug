c101ccb7df9fdf8dd3de96954a01c0e05a155f13
Damilare Elijah ISMAILA <bestbynature2010@gmail.com>
2023-12-10 13:34:16 +0000

    feat: Added showSocialAuth flag
    Added a flag showSocialAuth with value true to the routes.js file.

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const passport = require("passport");
 
 module.exports = function (app, myDataBase) {
-
   function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
@@ -13,6 +12,7 @@ module.exports = function (app, myDataBase) {
     res.render("index", {
       showLogin: true,
       showRegistration: true,
+      showSocialAuth: true,
       title: "Connected to Database",
       message: "Please login",
     });
@@ -27,39 +27,38 @@ module.exports = function (app, myDataBase) {
       },
     );
 
-    app.route("/profile").get(ensureAuthenticated, (req, res) => {
-      res.render("profile", { username: req.user.username });
-    });
-  
-    app.route("/logout").get((req, res) => {
-      req.logout();
-      res.redirect("/");
-    });
-  
-    app.route("/register").post(
-      (req, res, next) => {
-        myDataBase.findOne({ username: req.body.username }, (err, user) => {
-          if (err) next(err);
-          else if (user) res.redirect("/");
-          else {
-            const hash = bcrypt.hashSync(req.body.password, 12);
-            myDataBase.insertOne(
-              {
-                username: req.body.username,
-                password: hash,
-              },
-              (err, doc) => {
-                if (err) res.redirect("/");
-                else next(null, doc.ops[0]);
-              },
-            );
-          }
-        });
-      },
-      passport.authenticate("local", { failureRedirect: "/" }),
-      (req, res, next) => {
-        res.redirect("/profile");
-      },
-    );
-  
-}
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render("profile", { username: req.user.username });
+  });
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) next(err);
+        else if (user) res.redirect("/");
+        else {
+          const hash = bcrypt.hashSync(req.body.password, 12);
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: hash,
+            },
+            (err, doc) => {
+              if (err) res.redirect("/");
+              else next(null, doc.ops[0]);
+            },
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    },
+  );
+};
