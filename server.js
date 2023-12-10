93f96a955cf0e156fc9b548613fd2bc384ca2519
Dammy <bestbynature2010@gmail.com>
2023-12-10 12:50:48 +0100

    Add registration route

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ myDB(async (client) => {
   app.route("/").get((req, res) => {
     res.render("index", {
       showLogin: true,
+      showRegistration: true,
       title: "Connected to Database",
       message: "Please login",
     });
@@ -69,6 +70,27 @@ myDB(async (client) => {
     req.logout();
     res.redirect("/");
   });
+
+  app.route("/register").post(
+    (req, res, next)=>{
+      myDataBase.findOne({username: req.body.username}, (err, user)=>{
+        if(err) next(err);
+        else if(user) res.redirect('/');
+        else{
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          }, (err, doc)=>{
+            if(err) res.redirect('/');
+            else next(null, doc.ops[0]);
+          })
+        }
+      })
+    }, passport.authenticate('local', {failureRedirect: '/'}), (req, res, next)=>{
+      res.redirect('/profile');
+    }
+  )
+
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
