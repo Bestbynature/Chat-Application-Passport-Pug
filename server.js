db65b86329fae9e3faa06dccb539b7ec1efd2bc8
Dammy <bestbynature2010@gmail.com>
2023-12-10 12:44:32 +0100

    Add 404 route and logout logic

--- server.js
+++ server.js
@@ -65,6 +65,15 @@ myDB(async (client) => {
       res.render("profile", { username: req.user.username });
     });
 
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
+  });
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
