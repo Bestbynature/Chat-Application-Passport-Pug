b236ea6210ed0173ea33d0564be7eecb2924d516
Dammy <bestbynature2010@gmail.com>
2023-12-10 12:28:26 +0100

    Add the ensure authentiction middleware

--- server.js
+++ server.js
@@ -53,9 +53,17 @@ myDB(async (client) => {
       },
     );
 
-  app.route("/profile").get((req, res) => {
-    res.render("profile", { username: req.user.username });
-  });
+    function ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect("/");
+    }
+
+  app.route("/profile")
+    .get(ensureAuthenticated, (req, res) => {
+      res.render("profile", { username: req.user.username });
+    });
 
   app.use(
     session({
